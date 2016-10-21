38d195deb631d5cca15613799cda345b8306abb7
Puneet Sharma <puneet.s2004@gmail.com>
2016-10-21 19:54:17 -0400

    First

---
+++ src/hello.erl
@@ -0,0 +1,14 @@
+%%%-------------------------------------------------------------------
+%%% @author Puneet Sharma
+%%% @copyright (C) 2016, Stevens Institute of Technology
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. Oct 2016 19:51
+%%%-------------------------------------------------------------------
+-module(hello).
+-author("Puneet Sharma").
+
+%% API
+-export([hello/0]).
+hello()->io:format("Hello World!").
